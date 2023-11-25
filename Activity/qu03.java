147bf9620fd072072525350cc80a4c043a580035
MOHAMMED JUMAIL <114874696+JUMAIL-WM@users.noreply.github.com>
2023-11-25 17:31:30 +0530

    Add files via upload

---
+++ Activity/qu03.java
@@ -0,0 +1,12 @@
+public class qu03
+{
+    public static void main(String[] args)
+    {
+      int sum = 0;
+      for(int i=1; i <= 500;i++)
+      {
+          sum +=i;
+      }
+        System.out.println("Sum of natural numbers within 500: " + sum);
+    }
+}
